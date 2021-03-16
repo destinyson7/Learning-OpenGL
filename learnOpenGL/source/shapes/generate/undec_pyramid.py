d3be1fafce8fa9dec415fbd0db68dc07cbc8eb85
destinyson7 <tanish.lad@research.iiit.ac.in>
2021-03-16 09:51:02 +0530

    fixed undecagonal pyramid - made it closed, before it was open

--- learnOpenGL/source/shapes/generate/undec_pyramid.py
+++ learnOpenGL/source/shapes/generate/undec_pyramid.py
@@ -21,7 +21,7 @@ for i in range(1, 11):
 vertices.append(centre)
 
 # print(len(vertices))
-print(33)
+print(66)
 
 for i in range(11):
 
@@ -30,6 +30,18 @@ for i in range(11):
 
     print(
         f'{round(vertices[11][0], 10)} {round(vertices[11][2], 10)} {round(vertices[11][1], 10)} {color}')
+
+    print(
+        f'{round(vertices[i][0], 10)} {round(vertices[i][2], 10)} {round(vertices[i][1], 10)} {color}')
+
+    print(
+        f'{round(vertices[(i+1)%11][0], 10)} {round(vertices[(i+1)%11][2], 10)} {round(vertices[(i+1)%11][1], 10)} {color}')
+
+color = str(round(np.random.random(), 10)) + " " + str(round(np.random.random(),
+                                                             10)) + " " + str(round(np.random.random(), 10)) + " "
+for i in range(11):
+    print(
+        f'{round(0, 10)} {round(0, 10)} {round(0, 10)} {color}')
 
     print(
         f'{round(vertices[i][0], 10)} {round(vertices[i][2], 10)} {round(vertices[i][1], 10)} {color}')
